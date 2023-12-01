db432f1837874b7c3651ffa1a1b82b980e648a9c
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-01 15:45:44 +0300

    better implementation

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -26,72 +26,18 @@ fn part1(input: &str) -> i32 {
         .map(|(a, b)| 10 * b + a)
         .sum()
 }
-
-fn value_of_digit(digit: &str) -> i32 {
-    match digit {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => unreachable!(),
-    }
-}
-
 fn part2(input: &str) -> i32 {
-    let digits_as_str = [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ];
-    let mut new_input = String::new();
-    for line in input.lines() {
-        let mut min_index = usize::MAX;
-        let mut max_index = usize::MIN;
-        let mut min_match = "";
-        let mut max_match = "";
-        let mut new_line = String::from(line) + "\n";
-        for digit in digits_as_str.iter() {
-            let index1 = match line.find(digit) {
-                Some(i) => i,
-                None => continue,
-            };
-            let index2 = match line.rfind(digit) {
-                Some(i) => i,
-                None => continue,
-            };
-            if index1 < min_index {
-                min_index = index1;
-                min_match = digit;
-            }
-            if index2 > max_index {
-                max_index = index2;
-                max_match = digit;
-            }
-            if min_index == usize::MAX {
-                continue;
-            } else if max_index == min_index {
-                new_line = format!(
-                    "{}{}{}\n",
-                    line[..min_index].to_string(),
-                    value_of_digit(min_match),
-                    line[min_index..].to_string()
-                );
-            } else {
-                new_line = format!(
-                    "{}{}{}{}{}\n",
-                    line[..min_index].to_string(),
-                    value_of_digit(min_match),
-                    line[min_index..max_index].to_string(),
-                    value_of_digit(max_match),
-                    line[max_index..].to_string()
-                );
-            }
-        }
-        new_input.push_str(&new_line);
-    }
+    let mut new_input = input.to_string();
+    new_input = new_input.replace("one", "o1e");
+    new_input = new_input.replace("two", "t2o");
+    new_input = new_input.replace("three", "t3e");
+    new_input = new_input.replace("four", "f4r");
+    new_input = new_input.replace("five", "f5e");
+    new_input = new_input.replace("six", "s6x");
+    new_input = new_input.replace("seven", "s7n");
+    new_input = new_input.replace("eight", "e8t");
+    new_input = new_input.replace("nine", "n9e");
+
     part1(&new_input)
 }
 
