f65d8768d7bcca89a21d7e3f78c599fd7c30e734
BahadirAydin <bahadiraydin1923@gmail.com>
2023-12-08 13:50:28 +0300

    day08

---
+++ day08/src/main.rs
@@ -0,0 +1,71 @@
+use lazy_static::lazy_static;
+use num::integer::lcm;
+use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
+
+lazy_static! {
+    static ref RE: Regex = Regex::new(r"([A-Z]){3}").unwrap();
+}
+
+fn part1(instructions: &Vec<char>, map: &HashMap<&str, (&str, &str)>) -> i32 {
+    let mut curr = "AAA";
+    let mut pos = 0;
+    let mut step = 0;
+
+    while curr != "ZZZ" {
+        let (a, b) = map.get(curr).unwrap();
+
+        if instructions[pos] == 'R' {
+            curr = b;
+        } else {
+            curr = a;
+        }
+        pos = (pos + 1) % instructions.len();
+        step += 1;
+    }
+
+    step
+}
+
+fn part2(instructions: &Vec<char>, map: &HashMap<&str, (&str, &str)>) -> i64 {
+    let starts = map.keys().filter(|k| k.ends_with('A')).collect::<Vec<_>>();
+    let steps = starts.iter().map(|s| {
+        let mut curr = *s;
+        let mut step = 0;
+        let mut pos = 0;
+        while !curr.ends_with('Z') {
+            let (a, b) = map.get(curr).unwrap();
+            if instructions[pos] == 'R' {
+                curr = b;
+            } else {
+                curr = a;
+            }
+            pos = (pos + 1) % instructions.len();
+            step += 1;
+        }
+        step
+    });
+    steps.fold(1, lcm)
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let mut iter = input.lines();
+    let instructions = iter.next().unwrap().chars().collect::<Vec<_>>();
+    let mut map = HashMap::new();
+
+    iter.skip(1).for_each(|line| {
+        let caps = RE.captures_iter(line).collect::<Vec<_>>();
+        map.insert(
+            caps[0].get(0).unwrap().as_str(),
+            (
+                caps[1].get(0).unwrap().as_str(),
+                caps[2].get(0).unwrap().as_str(),
+            ),
+        );
+    });
+
+    println!("Part 1: {}", part1(&instructions, &map));
+    println!("Part 2: {}", part2(&instructions, &map));
+}
